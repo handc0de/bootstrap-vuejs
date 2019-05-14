11fea395d75333eb2280abd370951b837b362a83
handc0de <handc0de8@gmail.com>
2019-05-14 13:35:36 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go Asd</a>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../assets/custom-bootstrap.scss';
+@import '../../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
